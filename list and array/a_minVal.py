de032726c6467644d1a9c66fb652732e79682530
Anas <gazianas2020@gmail.com>
2025-09-17 21:09:25 +0600

    Add files via upload

---
+++ list and array/a_minVal.py
@@ -0,0 +1,7 @@
+arr = [20,55,98,25,19,20]
+minVal =arr[0]
+for x in arr:
+  if x< minVal:
+    minVal = x
+
+    print(minVal)
---
+++ list and array/b_maxVal.py
@@ -0,0 +1,7 @@
+arr = [20,55,98,25,19]
+
+maxVal = arr[0]
+for x in arr:
+  if x> maxVal:
+    maxVal = x
+print(maxVal, end='')
